a9aa14e950d744927ff209bdda3f4681f000bda1
Piovsk <arthur.quaglio00@gmail.com>
2025-05-09 12:58:57 -0300

    SellerDaoJSBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +12,9 @@ public class Program {
     public static void main(String[] args) {
         Department d = new Department(1, "Books");
         Seller s = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, d);
+
+
+        SellerDao sllerdao = DaoFactory.createSellerDao();
 
         System.out.println(s);
 
